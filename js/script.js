feb1a485eb432fb07ea0585f738acb8730b280cf
Kiran K Kumar <kirankumarkendole@gmail.com>
2015-09-12 07:18:04 +0530

    Added ASC & DES sorting to the Grid.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 var grid = angular.module("gridModule",["kendo.directives"]);
 angular.module("AngularModule",["gridModule","accordionApp"]);
 var accordian = angular.module("accordionApp",[]);
+
 accordian.controller("SummaryController" , function($scope){
-	
+	$scope.init = function () {
+		$('.sd-block').click(function(){
+		   	if(! $(this).hasClass('active') ){
+		   		$('.sd-block').removeClass('active');
+		   		$(this).addClass('active');
+		   		if( $(this).hasClass('summary') ) {
+			    	$("#details-grid").removeClass("mytask").addClass("summary");
+			    }
+			    else if ($(this).hasClass('mytask') ) {
+			    	$("#details-grid").removeClass("summary").addClass("mytask");
+			    }
+		 	}
+		});
+	}
 });
 
-grid.controller("GridCtrl", function($scope){
+grid.controller("mytaskGridCtrl", function($scope){
 	$scope.tooltipOpt = {"a":"Approve", "m":"Modify", "r":"Reject", "position":"top"};
 	$scope.mainGridOptions = {
 		dataSource: [ 
@@ -52,11 +66,16 @@ grid.controller("GridCtrl", function($scope){
 					 DateTime: "Originator Name"
 					}],
 		height: 180,
-		sortable: false,
+		sortable: true,
 		selectable: "row",
 		scrollable: false,
+		columnMenu: {
+			columns: false,
+			sortable: true
+		},
 		filterable: {
-			extra: false
+			extra: false,
+			mode: "menu"
         },
 		pageable: {
 			refresh: true,
@@ -101,12 +120,13 @@ grid.controller("GridCtrl", function($scope){
 		}],
 		dataBinding: function(e) {
 				var $pagerRefresh = $('.k-pager-refresh').clone();
-				$('#details-grid .k-pager-refresh').remove();
-				$('#details-grid .k-grid-header tr th:first-child').prepend($pagerRefresh);
+				var $grid = $('#details-grid.mytask');
+				//$('.k-pager-refresh').remove();
+				$grid.find('.k-grid-header tr th:first-child').prepend($pagerRefresh);
 
-				$('.k-pager-wrap').wrapInner('<div class="k-pager-innerwrap"></div>');
-				var grid = $('#details-grid');
-				grid.on('click','.scheck',function(){
+				$grid.find('.k-pager-wrap').wrapInner('<div class="k-pager-innerwrap"></div>');
+
+				$grid.on('click','.scheck',function(){
 					if( this.checked ){
 						$(this).closest('tr').addClass('active');
 						$('.approval-buttons').addClass('active');
@@ -116,9 +136,9 @@ grid.controller("GridCtrl", function($scope){
 							$('.approval-buttons').removeClass('active');
 						}
 					}
-				});
+				})
 				
-				grid.on('click','.comment-btn',function(){		
+				$grid.on('click','.comment-btn',function(){		
 					$('.commentbox,.sm-overlay').show();
 					$('.sm-overlay,.sum-lightbox .buttons button').click(function(){
 						$('.commentbox,.sm-overlay').hide()
@@ -128,6 +148,122 @@ grid.controller("GridCtrl", function($scope){
 	};
 
 	
+});
+
+	/* summary ctrl */
+	grid.controller("summaryGridCtrl", function($scope){
+	$scope.mainGridOptions = {
+		dataSource: [ 
+					{ ProcessId: "123456789876456",
+					 Description: "Description Info Here",
+					 Originator: "Originator Name",
+					 Concept: "Offer",
+					 Type: "Originator Name",
+					 DateTime: "Originator Name"
+					},
+					{ ProcessId: "123456789876456",
+					 Description: "Description Info Here",
+					 Originator: "Originator Name",
+					 Concept: "Offer",
+					 Type: "Originator Name",
+					 DateTime: "Originator Name"
+					},
+					{ ProcessId: "123456789876456",
+					 Description: "Description Info Here",
+					 Originator: "Originator Name",
+					 Concept: "Offer",
+					 Type: "Originator Name",
+					 DateTime: "Originator Name"
+					},
+					{ ProcessId: "123456789876456",
+					 Description: "Description Info Here",
+					 Originator: "Originator Name",
+					 Concept: "Offer",
+					 Type: "Originator Name",
+					 DateTime: "Originator Name"
+					},
+					{ ProcessId: "123456789876456",
+					 Description: "Description Info Here",
+					 Originator: "Originator Name",
+					 Concept: "Offer",
+					 Type: "Originator Name",
+					 DateTime: "Originator Name"
+					},
+					{ ProcessId: "123456789876456",
+					 Description: "Description Info Here",
+					 Originator: "Originator Name",
+					 Concept: "Offer",
+					 Type: "Originator Name",
+					 DateTime: "Originator Name"
+					}],
+		height: 180,
+		sortable: true,
+		selectable: "row",
+		scrollable: false,
+		columnMenu: {
+			columns: false
+		},
+		filterable: {
+			extra: false
+        },
+		pageable: {
+			refresh: true,
+			pageSizes: false,
+			buttonCount: 2,
+			pageSize: 5,
+			info: true,
+			messages: {
+			  display: " {2}"
+			}
+		},
+		dataBound: function() {
+			this.expandRow(this.tbody.find("tr.k-master-row"));
+		},
+		columns: [
+		{
+			field: "ProcessId",
+			title: "ProcessId",
+			width: 200
+		}, {
+			field: "Description",
+			title: "Description"
+		}, {
+			field: "Originator",
+			title: "Originator"
+		}, {
+			field: "Description",
+			title: "Description"
+		}, {
+			field: "Concept",
+			title: "Concept"
+		}, {
+			field: "Type",
+			title: "Type"
+		}, {
+			field: "DateTime",
+			title: "Date / Time"
+		}],
+		dataBinding: function(e) {
+				var $pagerRefresh = $('.k-pager-refresh').clone();
+				var $grid = $('#details-grid.summary');
+				$('.k-pager-refresh').remove();
+				$grid.find('.k-grid-header tr th:first-child').prepend($pagerRefresh);
+
+				$grid.find('.k-pager-wrap').wrapInner('<div class="k-pager-innerwrap"></div>');
+
+				$grid.on('click','.scheck',function(){
+					if( this.checked ){
+						$(this).closest('tr').addClass('active');
+						$('.approval-buttons').addClass('active');
+					} else {
+						$(this).closest('tr').removeClass('active');
+						if( $('.scheck:checked').length == 0 ) {
+							$('.approval-buttons').removeClass('active');
+						}
+					}
+				})
+		}
+	};
 });
 
 
